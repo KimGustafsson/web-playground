a50d374640c8a1899f78eb0fdd7f2a464f41792c
Kim Gustafsson <Kim.Gustafsson@cybercom.com>
2019-06-15 14:29:29 +0200

    Add three js to play with

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import * as THREE from 'three';
+import './style.css';
+
+// Register a service worker to make the app work offline
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js').then(registration => {
@@ -8,12 +12,21 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-function component() {
-   const element = document.createElement('div');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-   element.innerHTML = '<h1>Hello World!</h1>'
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({
+  color: 0xff0051,
+});
+let cube = new THREE.Mesh(geometry, material);
 
-   return element;
- }
+scene.add(cube);
+camera.position.z = 5;
+renderer.render(scene, camera);
 
- document.body.appendChild(component());
+document.body.appendChild(renderer.domElement);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
    devServer: {
       contentBase: './dist'
    },
+   module: {
+      rules: [{
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+      }, ],
+   },
    plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
